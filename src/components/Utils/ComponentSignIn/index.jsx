f51a27dbf168e3539c12902c40750ffcde7d27bf
HermesonxN <hermesoninfo@gmail.com>
2024-04-29 12:18:47 -0300

    Removing google component login

--- src/components/Utils/ComponentSignIn/index.jsx
+++ src/components/Utils/ComponentSignIn/index.jsx
@@ -3,36 +3,13 @@ import { FcGoogle } from "react-icons/fc";
 import { BsFacebook } from "react-icons/bs";
 import { FaApple } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
-import GoogleLogin from "react-google-login";
 import './style.css';
 
 function ComponentSignIn(props){
-
-    // const responseGoogle = (response) => {
-    //     console.log(response)
-    // }
-
     return(
         <div>
             <div className="components">
                 <div className="componentDiv">
-                    {/* <GoogleLogin
-                        clientId='4748182425-as5qha965m3e79qtv4vjhho53q3aqnaj.apps.googleusercontent.com'
-                        onSuccess={responseGoogle}
-                        onFailure={responseGoogle}
-                        cookiePolicy={'single_host_origin'}
-                        render={renderProps => (
-                            <button
-                                className="google-login-button"
-                                onClick={renderProps.onClick}
-                                disabled={renderProps.disabled}
-                            >
-                                <FcGoogle className="icon" size={23}/>
-                                <a className="componentText">{props.text} com Google</a>
-                            </button>
-                          )}
-                    /> */}
-
                     <FcGoogle className="icon" size={23}/>
                     <a className="componentText">Entre com Google</a>
                 </div>
